3bb2dbc334b4dc7781c8f26ae8d3ad70477021f0
stelo <contact@stelo.dev>
2026-09-14 16:42:43 -0700

    fix: disable rosetta

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
           {
             nix-homebrew = {
               enable = true;
-              enableRosetta = true;
+              enableRosetta = false;
               user = "stelo";
             };
           }
